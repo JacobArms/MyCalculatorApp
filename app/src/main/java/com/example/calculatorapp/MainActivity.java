05067c77e330dbdf97ac8bd556999820ef153c7a
JacobArms <90772750+JacobArms@users.noreply.github.com>
2022-08-23 09:24:19 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -13,6 +13,20 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void Calculate(){
-
+        Intent intent = getIntent();
+        String showName = intent.getStringExtra("");
+        TextView firstNum = findViewById(R.firstNumber);
+        TextView secondNum = findViewById(R.secondNumber);
+        String numOne = firstNum.getText().toString();
+        String numTwo = secondNum.getText().toString();
+        int number1 = Integer.parseInt(numOne);
+        int number2 = Integer.parseInt(numTwo);
+        int solution;
+        if(){
+            
+        }
+        String wordSolution = ("" + solution);
+        EditText editText = (EditText) findViewById(R.id.answer);
+        editText.setText(wordSolution);
     }
 }
