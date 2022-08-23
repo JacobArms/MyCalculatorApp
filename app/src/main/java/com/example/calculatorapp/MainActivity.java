a68fd94d1289f0e71943f03942c695feb45ea7b4
JacobArms <90772750+JacobArms@users.noreply.github.com>
2022-08-23 18:55:07 -0500

    Added a new onClick to move the image back and forth

--- app/src/main/java/com/example/calculatorapp/MainActivity.java
+++ app/src/main/java/com/example/calculatorapp/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.calculatorapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.TranslateAnimation;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    // FOUND ONLINE
+    // Once the Easter Egg button is clicked at the bottom of the page,
+    // the image will move left to right and then right to left
+    public void imageClick(View view){
+        //Grabs the image of Dr Heinz
+        ImageView imageView = (ImageView) findViewById(R.id.drHeinz);
+        //Sets the xFrom, xTo, yFrom, and yTo
+        TranslateAnimation animation = new TranslateAnimation(-160.0f,160.0f, 0.0f, 0.0f);
+        animation.setDuration(800);    //animation duration, impacts the speed of the animation as well
+        animation.setRepeatCount(5);    //Sets the repeat count
+        animation.setRepeatMode(2);     //Repeat animation from (left to right, right to left)
+        imageView.startAnimation(animation);    //Starts the animation of the image on the page
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
